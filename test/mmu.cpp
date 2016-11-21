9d00e92aa09cd012bdb12c295489505459444956
Jeremias Yehdegho <j.yehdegho@gmail.com>
2016-11-21 16:26:12 +0100

    list initialization for vector

--- test/mmu.cpp
+++ test/mmu.cpp
@@ -6,7 +6,7 @@
 
 SCENARIO("reading a WORD should have correct endianness", "[mmu]") {
 	GIVEN("a MMU containing a RomOnly containing a BYTE vector") {
-		std::vector<BYTE> v{0x0, 0xff, 0xaa, 0xbb};
+		std::vector<BYTE> v = {0x0, 0xff, 0xaa, 0xbb};
 		auto ptr = std::make_unique<RomOnly>(std::move(v));
 		MMU mmu{std::move(ptr)};
 
