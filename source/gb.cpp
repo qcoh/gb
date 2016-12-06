5ac086a6f1c64bd46ccf685b05275bccb8310784
Jeremias Yehdegho <j.yehdegho@gmail.com>
2016-12-06 16:57:31 +0100

    use cxxopts for commandline args

--- source/gb.cpp
+++ source/gb.cpp
@@ -1,15 +1,22 @@
 #include <iostream>
 #include <exception>
 
+#include "cxxopts.hpp"
 #include "mapper.h"
 
-int main(int args, char *argv[]) {
-	if (args != 2) {
-		std::cout << "gb [rom]\n";
-		return 0;
-	}
+int main(int argc, char *argv[]) {
+	cxxopts::Options options{"gb", "A GameBoy emulator"};
+	options.add_options()
+		("d,debug", "Enable debugging")
+		("f,file", "File name", cxxopts::value<std::string>());
+	options.parse(argc, argv);
+
 	try {
-		auto mapper = Mapper::fromFile(argv[1]);
+		auto filename = options["f"].as<std::string>();
+		if (filename == "") {
+			throw std::runtime_error{"No rom"};
+		}
+		auto mapper = Mapper::fromFile(filename);
 		(void)mapper;
 	} catch (std::exception& e) {
 		std::cerr << e.what() << '\n';
