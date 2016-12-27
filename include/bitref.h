81e8273730ecb7c720e19d2090d093081643d265
Jeremias Yehdegho <j.yehdegho@gmail.com>
2016-12-27 17:42:57 +0100

    bitref derives from ref

--- include/bitref.h
+++ include/bitref.h
@@ -1,20 +1,19 @@
 #pragma once
 
-#include <functional>
+#include "ref.h"
 
 // modelled after reference_wrapper (http://en.cppreference.com/w/cpp/utility/functional/reference_wrapper)
 template <typename T, int I>
-class BitRef {
+class BitRef : public Ref<bool> {
 	public:
-		BitRef(T& ref) noexcept : ptr{std::addressof(ref)} {}
-		BitRef(T&&) = delete;
-		BitRef(const BitRef&) noexcept = default;
-		BitRef& operator=(const BitRef&) noexcept = default;
-		operator bool () const noexcept {
+		BitRef(T& ref) : ptr{&ref} {}
+		BitRef(const BitRef&) = default;
+		BitRef& operator=(const BitRef&) = default;
+		virtual operator bool () const override {
 			return (((*ptr >> I) & 1) != 0);
 		}
 		// TODO: return type?
-		void operator=(bool rhs) noexcept {
+		virtual void operator=(bool rhs) override {
 			*ptr = (*ptr & (~(1 << I))) | static_cast<T>(rhs << I);
 		}
 	private:
